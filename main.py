42e9464ce4593d0ffcace5831c16eb5d96dce422
Maximilian-Frisch <frischmaximilian@gmail.com>
2016-12-07 23:42:47 -0500

    'update'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 import jinja2
 
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), 'www')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class Handler(webapp2.RequestHandler):
