e903a0a46d076bbaf3cb7bd2a62cda4a43a290ce
Maximilian-Frisch <frischmaximilian@gmail.com>
2016-12-08 00:06:51 -0500

    'update'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 import jinja2
 
 
-template_dir = os.path.join(os.path.dirname(__file__), 'www')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 class Handler(webapp2.RequestHandler):
@@ -18,7 +18,14 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
         
+class Ministries(Handler):
 
+    def render_front(self, error=""):
+        self.render("ministries.html", error = error)
+        
+    def get(self):
+        self.render_front()
+       
         
 class MainPage(Handler):
     
